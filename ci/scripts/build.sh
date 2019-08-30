9f2eee1e18e07f7cd553a84b53f0569fe7213459
Patrick Ugwu <patrick.ugwu@yoti.com>
2019-08-30 15:30:15 +0100

    removed the input of built-university-resource.

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -7,4 +7,7 @@ mvn clean install -Dspring.profiles.active=dev
 
 cd ../
 
+echo "start to copy files to the folder built-university-resource"
 cp -R university-resource/* built-university-resource
+echo "finished copying files"
+ls built-university-resource
