09e5c46620868a0b9ff672102bb929793a5737dd
Patrick Ugwu <patrick.ugwu@yoti.com>
2019-08-30 10:53:06 +0100

    removed the line for copying the target folder.

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -6,6 +6,6 @@ mvn clean install -Dspring.profiles.active=dev
 
 mkdir built-university-resource
 
-mv /target built-university-resource/
+#mv /target built-university-resource/
 mv Dockerfile built-university-resource/
 mv docker-compose.yml built-university-resource
