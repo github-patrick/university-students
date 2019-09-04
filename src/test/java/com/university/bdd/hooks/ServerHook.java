55a8431a4f9afa43f023672434f24f832e413af3
Patrick Ugwu <patrick.ugwu@yoti.com>
2019-09-04 18:00:47 +0100

    Changed files to run docker in docker.

--- src/test/java/com/university/bdd/hooks/ServerHook.java
+++ src/test/java/com/university/bdd/hooks/ServerHook.java
@@ -3,23 +3,16 @@ package com.university.bdd.hooks;
 import cucumber.api.java.Before;
 import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Slf4j
 public class ServerHook {
 
-//    @LocalServerPort
     private int port = 8888;
 
     @Before
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-        RestAssured.baseURI = "http://192.168.99.100";
+        RestAssured.baseURI = "http://localhost";
         RestAssured.port = this.port;
     }
 
