dbcd0bee8a2cfe25f2f8db2259255cd73d885a20
Patrick Ugwu <patrick.ugwu@yoti.com>
2019-09-08 20:31:27 +0100

    shortened the pipeline time

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -4,3 +4,9 @@ set -e
 cd university-resource
 
 mvn clean install -Dspring.profiles.active=dev
+
+cd ../
+echo "start to copy files to the folder built-university-resource"
+cp -R university-resource/* artificaft-for-image
+echo "finished copying files"
+ls artificaft-for-image
