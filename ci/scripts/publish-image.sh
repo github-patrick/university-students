f3074e0a9ffeae41f9737ed77759914d46933392
Patrick Ugwu <patrick.ugwu@yoti.com>
2019-09-08 20:46:28 +0100

    added an executable run.sh reference

--- ci/scripts/publish-image.sh
+++ ci/scripts/publish-image.sh
@@ -2,14 +2,14 @@
  set -e
 
 echo publishing the image..........
-ls -l university-resource
-
-cd university-resource
-mvn clean install -DskipTests
-
-cd ../
-
-echo "start to copy files to the folder built-university-resource"
-cp -R university-resource/* built-university-resource
-echo "finished copying files"
-ls built-university-resource
+#ls -l university-resource
+#
+#cd university-resource
+#mvn clean install -DskipTests
+#
+#cd ../
+#
+#echo "start to copy files to the folder built-university-resource"
+#cp -R university-resource/* built-university-resource
+#echo "finished copying files"
+#ls built-university-resource
